a7dc44b8923bfa75581af8fd8b80113b5bbd33ad
Yiming Sun <yimsun@umail.iu.edu>
2012-12-07 21:29:16 +0000

    forgot to call another script file to load all jars onto the classpath

--- reingest.sh
+++ reingest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 . ./setpropfile.sh
+LOCALCLASSPATH=`/bin/sh $PWD/classpath.sh run`
 
 java -cp "build/classes:." -DPROPERTIES_LOCATION=$PROPERTIES_PATH edu.indiana.d2i.htrc.ingest.tools.Reingester $*
