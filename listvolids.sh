3f5306225acc5fa346718ec291d2f1cb8ebe1601
Yiming Sun <yimsun@umail.iu.edu>
2012-12-07 21:34:57 +0000

    fixed another mistake in the script file that after calling the other script to get the classpath, the variable holding the classpath was not passed to the jvm.

--- listvolids.sh
+++ listvolids.sh
@@ -4,5 +4,5 @@
 LOCALCLASSPATH=`/bin/sh $PWD/classpath.sh run`
 
 
-java -cp "build/classes:." -DPROPERTIES_LOCATION=$PROPERTIES_PATH edu.indiana.d2i.htrc.ingest.tools.KeyLister $*
+java -cp $LOCALCLASSPATH -DPROPERTIES_LOCATION=$PROPERTIES_PATH edu.indiana.d2i.htrc.ingest.tools.KeyLister $*
 
--- reingest.sh
+++ reingest.sh
@@ -3,4 +3,4 @@
 . ./setpropfile.sh
 LOCALCLASSPATH=`/bin/sh $PWD/classpath.sh run`
 
-java -cp "build/classes:." -DPROPERTIES_LOCATION=$PROPERTIES_PATH edu.indiana.d2i.htrc.ingest.tools.Reingester $*
+java -cp $LOCALCLASSPATH -DPROPERTIES_LOCATION=$PROPERTIES_PATH edu.indiana.d2i.htrc.ingest.tools.Reingester $*
